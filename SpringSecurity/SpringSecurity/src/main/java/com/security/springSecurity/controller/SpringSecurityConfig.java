09087b30185a98e8a89636633235bdc6003baea8
sundar-chilaka <sundar3.ch@gmail.com>
2025-01-27 13:01:42 +0530

    1.config class adding InMemoryUserDetailsManager 2.SpringSecurityController class adding Roles like User,Admin,Sundar

--- SpringSecurity/SpringSecurity/src/main/java/com/security/springSecurity/controller/SpringSecurityConfig.java
+++ SpringSecurity/SpringSecurity/src/main/java/com/security/springSecurity/controller/SpringSecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.SneakyThrows;
@@ -13,6 +16,27 @@ import lombok.SneakyThrows;
 @EnableWebSecurity
 public class SpringSecurityConfig {
 	
+	@Bean
+	public InMemoryUserDetailsManager userDetailsManager() {
+		
+		
+		UserDetails u1 = 
+				User.withDefaultPasswordEncoder()
+					.username("Sundar")	
+					.password("Sundar@123")
+					.build();
+		UserDetails u2 = 
+				User.withDefaultPasswordEncoder()
+					.username("ADMIN")	
+					.password("Admin@123")
+					.build();
+		UserDetails u3= 
+				User.withDefaultPasswordEncoder()
+					.username("USER")	
+					.password("User@123")
+					.build();
+		return new InMemoryUserDetailsManager(u1,u2,u3);
+	}
 	
 	@Bean
 	@SneakyThrows
--- SpringSecurity/SpringSecurity/src/main/java/com/security/springSecurity/controller/SpringSecurityController.java
+++ SpringSecurity/SpringSecurity/src/main/java/com/security/springSecurity/controller/SpringSecurityController.java
@@ -1,5 +1,7 @@
 package com.security.springSecurity.controller;
 
+import org.springframework.context.annotation.Role;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,16 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringSecurityController {
 
 	@GetMapping("/meg")
+	@PreAuthorize("hasRole('ADMIN')")
 	public String message() {
 		return "Wellcom to  Spring Security";
 	}
 
 	@GetMapping("/contactg")
+	@PreAuthorize("hasRole('Sundar')")
 	public String contact() {
 		return "ph + 9133774632";
 	}
 
 	@GetMapping("/welcome")
+	@PreAuthorize("hasRole('USER')")
 	public String welcome() {
 		return "Hello Sundar....";
 	}
